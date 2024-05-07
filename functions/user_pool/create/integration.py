76905584b75cc363f6114f16cd45234b90db93b9
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-07 10:05:04 -0300

    feat(user-pool): implement user pool creation function with unit and integration tests

---
+++ functions/user_pool/create/integration.py
@@ -0,0 +1,10 @@
+import pytest
+import requests
+from lambda_forge.constants import BASE_URL
+
+@pytest.mark.integration(method="GET", endpoint="/user_pool")
+def test_create_status_code_is_200():
+
+    response = requests.get(url=f"{BASE_URL}/user_pool")
+
+    assert response.status_code == 200 

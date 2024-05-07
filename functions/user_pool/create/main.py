9089ce445b975c84d91e2439d2273c4c17221d55
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-07 10:23:05 -0300

    feat(user_pool/create): implement pool creation logic in main

--- functions/user_pool/create/main.py
+++ functions/user_pool/create/main.py
@@ -1,18 +1,38 @@
 import json
 from dataclasses import dataclass
+from typing import List
+import uuid
+import os
+import boto3
+
+@dataclass
+class Pool:
+    domain: str
+    weight: int
 
 @dataclass
 class Input:
-    pass
+    pool: List[Pool]
 
 @dataclass
 class Output:
-    message: str
+    pool_id: str
 
 
 def lambda_handler(event, context):
 
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('POOL_TABLE_NAME')
+    table = dynamodb.Table(table_name)
+
+    pool = json.loads(event["body"])["pool"]
+    
+    pool_id = str(uuid.uuid4())
+    
+    for item in pool:
+        table.put_item(Item={"PK": pool_id, "SK": item["domain"], "weight": item["weight"]})
+        
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Hello World!"})
+        "body": json.dumps({"pool_id": pool_id})
     }
--- functions/user_pool/create/unit.py
+++ functions/user_pool/create/unit.py
@@ -1,8 +1,36 @@
 import json
 from .main import lambda_handler
 
-def test_lambda_handler():
+def test_lambda_handler(table):
 
-    response = lambda_handler(None, None)
+    event = {
+        "body": json.dumps({"pool":[
+            {
+                "domain": "example.com",
+                "weight": "1"
+            },
+            {
+                "domain": "example1.com",
+                "weight": "2"
+            },
+            {
+                "domain": "example2.com",
+                "weight": "3"
+            },
+            {
+                "domain": "example3.com",
+                "weight": "4"
+            }
+        ]})
+    }
 
-    assert response["body"] == json.dumps({"message": "Hello World!"})
+    response = lambda_handler(event, None)
+    pool_id = json.loads(response["body"])["pool_id"]
+    
+    pool = table.query(KeyConditionExpression="PK = :pk", ExpressionAttributeValues={":pk": pool_id})["Items"]
+    assert pool == [
+        {"PK": pool_id, "SK": "example.com", "weight": "1"},
+        {"PK": pool_id, "SK": "example1.com", "weight": "2"},
+        {"PK": pool_id, "SK": "example2.com", "weight": "3"},
+        {"PK": pool_id, "SK": "example3.com", "weight": "4"}
+    ]

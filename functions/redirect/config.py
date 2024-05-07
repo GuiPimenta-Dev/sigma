ca01daccb188bcd28dc3f821d73ef8c566915968
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-07 10:05:04 -0300

    feat(redirect-function): create redirect function with API endpoint, unit and integration tests

---
+++ functions/redirect/config.py
@@ -0,0 +1,15 @@
+from infra.services import Services
+
+class RedirectConfig:
+    def __init__(self, services: Services) -> None:
+
+        function = services.aws_lambda.create_function(
+            name="Redirect",
+            path="./functions/redirect",
+            description="A function to redirect based on user pool id",
+            
+        )
+
+        services.api_gateway.create_endpoint("GET", "/redirect", function, public=True)
+
+            
---
+++ functions/redirect/main.py
@@ -0,0 +1,38 @@
+import os
+import boto3
+import random
+from dataclasses import dataclass
+
+@dataclass
+class Input:
+    pass
+
+@dataclass
+class Output:
+    message: str
+
+
+def lambda_handler(event, context):
+    # Retrieving the DynamoDB table name from environment variable
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('POOL_TABLE_NAME')
+    table = dynamodb.Table(table_name)
+    
+    pool_id = event['queryStringParameters']['pool_id']
+    
+    # Retrieving the pool from DynamoDB
+    response = table.query(KeyConditionExpression="PK = :pk", ExpressionAttributeValues={":pk": pool_id})
+    pool = response['Items']
+    
+    # Extracting domains and weights from the pool
+    domains = [item['SK'] for item in pool]
+    weights = [int(item['weight']) for item in pool]
+    
+    # Choosing a domain based on the weights
+    selected_domain = random.choices(domains, weights=weights, k=1)[0]
+    
+    # Redirecting to the selected domain
+    redirect_url = f"https://{selected_domain}"
+
+    return {"statusCode": 301, "headers": {"Location": redirect_url}}
+

0bb01681fa50ede30e724bbc903a4c31e30fee91
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-05-07 10:36:00 -0300

    feat(user-pool-create): implement create functionality for user pool

--- functions/user_pool/create/config.py
+++ functions/user_pool/create/config.py
@@ -1,15 +1,11 @@
 from infra.services import Services
 
+
 class CreateConfig:
     def __init__(self, services: Services) -> None:
 
         function = services.aws_lambda.create_function(
-            name="Create",
-            path="./functions/user_pool",
-            description="Create User Pool Id",
-            directory="create"
+            name="Create", path="./functions/user_pool", description="Create User Pool Id", directory="create"
         )
 
         services.api_gateway.create_endpoint("GET", "/user_pool", function, public=True)
-
-            
--- functions/user_pool/create/main.py
+++ functions/user_pool/create/main.py
@@ -1,18 +1,22 @@
 import json
+import os
+import uuid
 from dataclasses import dataclass
 from typing import List
-import uuid
-import os
+
 import boto3
+
 
 @dataclass
 class Pool:
     domain: str
     weight: int
 
+
 @dataclass
 class Input:
     pool: List[Pool]
+
 
 @dataclass
 class Output:
@@ -21,18 +25,15 @@ class Output:
 
 def lambda_handler(event, context):
 
-    dynamodb = boto3.resource('dynamodb')
-    table_name = os.environ.get('POOL_TABLE_NAME')
+    dynamodb = boto3.resource("dynamodb")
+    table_name = os.environ.get("POOL_TABLE_NAME")
     table = dynamodb.Table(table_name)
 
     pool = json.loads(event["body"])["pool"]
-    
+
     pool_id = str(uuid.uuid4())
-    
+
     for item in pool:
         table.put_item(Item={"PK": pool_id, "SK": item["domain"], "weight": item["weight"]})
-        
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"pool_id": pool_id})
-    }
+
+    return {"statusCode": 200, "body": json.dumps({"pool_id": pool_id})}
